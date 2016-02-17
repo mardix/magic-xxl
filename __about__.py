f6aa93a7ff8febcdbdcd9c3ac42a18695fa2c39f
Mardix <mardix@pylot.io>
2016-02-17 02:11:41 -0500

    Tagged release: v0.3.1

--- __about__.py
+++ __about__.py
@@ -7,7 +7,7 @@ __all__ = [
 ]
 
 __title__ = "juicy-fruits"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __summary__ = "A collection librairies to work with Juice"
 __uri__ = "https://github.com/mardix/juicy-fruits"
 __author__ = "Mardix"
